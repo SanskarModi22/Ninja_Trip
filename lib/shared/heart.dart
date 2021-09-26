05b92ef5a1c29aecadd66406ccd8bd32aafa5bc2
Sanskar Modi <modisanskar5@gmail.com>
2021-09-27 01:53:31 +0530

    Completed Curves

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -9,15 +9,17 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation _colorAnimation;
   Animation _sizeAnimation;
+  Animation _curve;
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+    _curve = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_curve);
     _sizeAnimation = TweenSequence(<TweenSequenceItem<double>>[
       TweenSequenceItem(tween: Tween(begin: 30, end: 50), weight: 50),
       TweenSequenceItem(tween: Tween(begin: 50, end: 30), weight: 50),
-    ]).animate(_controller);
+    ]).animate(_curve);
   }
 
   @override
--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -8,16 +8,21 @@ class ScreenTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder(
-      tween: ColorTween(begin: Colors.blue, end: Colors.red),
+      tween: Tween<double>(begin: 0, end: 1),
       child: Text(
         text,
         style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
       ),
+      curve: Curves.easeInCubic,
       duration: Duration(seconds: 2),
-      builder: (BuildContext context, _val, _) {
-        return Text(
-          text,
-          style: TextStyle(fontSize: 36, color: _val, fontWeight: FontWeight.bold),
+      builder: (BuildContext context, _val, Widget child) {
+        return Opacity(
+          opacity: _val,
+          child: Padding(
+              padding: EdgeInsets.all(
+                _val * 20,
+              ),
+              child: child),
         );
       },
     );
